b6f27587d1369eb7fc70f16b2a010433abfa1d90
Nguyễn Mai Trường Lộc <locnmt@vietinfo.tech>
2021-11-30 17:52:06 +0700

    Update FakeLoginTestActivity.kt

--- app/src/main/java/com/application/dating/chat/FakeLoginTestActivity.kt
+++ app/src/main/java/com/application/dating/chat/FakeLoginTestActivity.kt
@@ -2,6 +2,7 @@ package com.application.dating.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.application.dating.R
 
 class FakeLoginTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
